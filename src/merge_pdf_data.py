4dbac917c793470fa519ea198bb68e8e7957a972
joey suen <joeysuen66@gmail.com>
2026-04-21 15:14:52 -0700

    Re-organize pre-processing, append year and source file column

--- src/merge_pdf_data.py
+++ src/merge_pdf_data.py
@@ -8,25 +8,12 @@ from pathlib import Path
 from rapidfuzz import process # for column name standardization
 
 # import data
-file_path = "data/raw/2026-03-15_extracted_data.xlsx"
-df = pd.read_excel(file_path, sheet_name=None) # note: df is a dictionary
+data_path = "data/raw/2026-03-15_extracted_data.xlsx"
+df = pd.read_excel(data_path, sheet_name=None) # note: df is a dictionary
+inflation = pd.read_csv("data/raw/inflation_adjust2024.csv")
+inflation_adjustment_map = inflation.set_index("base_yr")["adjust"].to_dict()
 
-# pre-processing
-for sheet_name, table in df.items():
-
-    # drop columns with "unnamed" column headers
-    df[sheet_name] = table.loc[:, ~table.columns.str.contains("^unnamed", case=False)]
-
-    # append file name as column
-    name = re.search(r"A.*?(?=\.pdf)", sheet_name, re.IGNORECASE)
-    df[sheet_name].loc[:, "file"] = name.group()
-
-    # standardize column names
-    df[sheet_name].columns = df[sheet_name].columns.str.lower().str.strip() # capitalization
-    df[sheet_name].columns = [re.sub(r'[^\w]', '_', col) for col in df[sheet_name].columns] # replace non-alphanumerics with underscore
-    df[sheet_name].columns = [re.sub(r'_+', '_', col).strip('_') for col in df[sheet_name].columns] # remove consecutive underscores
-
-# grouping similarly formatted PDFs
+# groupings of similarly formatted PDFs
 groups = {
     "group1": ["A58_31-en-table.pdf",
                "A60_ID6-en-table.pdf",
@@ -52,6 +39,29 @@ groups = {
                "A77_INF2-en.pdf (General)"]
 }
 
+# year mappings
+years = {"A58_31-en-table.pdf": 2005,
+        "A60_ID6-en-table.pdf": 2007,
+        "A61_ID1-en-table.pdf": 2008,
+        "A62_30-en.pdf": 2008,
+        "A63_33-en.pdf": 2009,
+        "A63_ID4-en.pdf (member states)": 2009,
+        "A63_ID4-en.pdf (donors)": 2009,
+        "A64_29Add1Corr1-en.pdf": 2010,
+        "A65_29Add1-en.pdf (General)": 2010,
+        "A66_29Add1-en.pdf (General)": 2012,
+        "A68_INF1-en.pdf (General)": 2014,
+        "A69_INF3-en.pdf (General)": 2015,
+        "A70_INF4-en.pdf  (General)": 2016,
+        "A71_INF2-en.pdf  (General)": 2017,
+        "A72_INF5-en.pdf  (General)": 2018,
+        "A73_INF3-eng.pdf  (General)": 2019,
+        "a74_inf4-en.pdf (General)": 2020,
+        "A75_INF5-en.pdf (General)": 2021,
+        "a76_inf2-en.pdf (General)": 2022,
+        "A77_INF2-en.pdf (General)": 2023
+    }
+
 # function: standardizing column names
 def fuzzy_rename(df, col_names, threshold=80):
     rename_map = {}
@@ -60,6 +70,42 @@ def fuzzy_rename(df, col_names, threshold=80):
         if score >= threshold and col != match:
             rename_map[col] = match
     return df.rename(columns=rename_map)
+
+# pre-processing
+for sheet_name, table in df.items():
+
+    table = table.copy()
+
+    # drop columns with "unnamed" column headers
+    table = table.loc[:, ~table.columns.str.contains("^unnamed", case=False)]
+
+    # standardize column names
+    table.columns = table.columns.str.lower().str.strip() # capitalization
+    table.columns = [re.sub(r'[^\w]', '_', col) for col in table.columns] # replace non-alphanumerics with underscore
+    table.columns = [re.sub(r'_+', '_', col).strip('_') for col in table.columns] # remove consecutive underscores
+
+    # append file name as column
+    name = re.search(r"A.*?(?=\.pdf)", sheet_name, re.IGNORECASE)
+    table["file"] = name.group()
+
+    # append year as column
+    if sheet_name in groups["group1"]: # group 1
+        assessment_cols = [col for col in table.columns if "assessment" in col.lower()]
+        if assessment_cols:
+            year = re.search(r"\d{4}", assessment_cols[0]).group()
+            table["year"] = int(year)
+    elif sheet_name in years:
+        table["year"] = years[sheet_name]
+
+    # adjust numbers for inflation (standardize to 2024 USD)
+    if ("year" in table.columns) and ("year" in range(2000, 2023)):
+        table_year = table["year"].iloc[0]
+        usd_cols = table.select_dtypes(include="number").columns.difference(["year"])
+        table[usd_cols] *= inflation_adjustment_map[table_year]
+
+    df[sheet_name] = table
+
+
 
 # GROUP 1
 def clean_group1(df=df, groups=groups):
@@ -78,24 +124,11 @@ def clean_group1(df=df, groups=groups):
     for sheet in groups["group1"]:
 
         # key all dataframes in group 1
-        table = df[sheet]
+        table = df[sheet].copy()
 
-        # append "year" column
-        assessment_cols = [col for col in table.columns if "assessment" in col.lower()]
-        if assessment_cols:
-            assessment_col = assessment_cols[0]
-            year = re.search(r"\d{4}", assessment_col).group()
-<<<<<<< Updated upstream:src/merge_pdf_data.py
-            table["year"] = int(year)
-
-=======
-            df[sheet]["year"] = int(year)
-        
->>>>>>> Stashed changes:src/peritz_who-pdfs_csv-merge.py
         # generalizing table column names
         rename_map = dict(zip(table.columns[:9], g1_cols[:9]))
-        table = table.rename(columns=rename_map)
-        df[sheet] = table
+        df[sheet] = table.rename(columns=rename_map)
 
     # concatenate group 1
     g1_concat = pd.concat([df[f] for f in groups["group1"]], ignore_index=True)
@@ -105,89 +138,34 @@ def clean_group1(df=df, groups=groups):
 
     return g1_concat
 
-# GROUP 2 --------------------------------------
+# GROUP 2 
+def clean_group2(df=df, groups=groups):
 
-# upon first inspection, these tables appear to be the same,
-# except A63_33-en.pdf contains additional information
+    # A63_33-en.pdf is just an updated version of A62_30-en.pdf, so we'll just drop A62
+    g2 = df["A63_33-en.pdf"].copy()
 
-# standarize column names (to allow for checking for overlap)
-df["A63_33-en.pdf"] = df["A63_33-en.pdf"].rename(columns = {
-    "balance_for_prior_years_as_on_31_december_2007": "balance_for_prior_years_31_dec_2007",
-    "rescheduled_assessment_as_on_31_december_2007": "rescheduled_assessment_31_dec_2007",
-    "collected_or_adjusted_during_current_biennium": "collected_adjusted_during_biennium",
-})
+    g2_cols = {
+            "members_and_associate_members": "members", 
+            "biennial_assessment_2008": "biennial_assessment_prev_year",
+            "biennial_assessment_2009": "biennial_assessment_curr_year",
+            "collected_or_adjusted_for_current_biennium_including_prepayment_2008": "collected_or_adjusted_including_prev_year_prepayment",
+            "collected_or_adjusted_for_current_biennium_including_prepayment_2009": "collected_or_adjusted_including_curr_year_prepayment",
+            "balance_for_prior_years_as_on_31_december_2007": "prior_years_balance",
+            "rescheduled_assessment_as_on_31_december_2007": "rescheduled_assessment_as_on_prev_year",
+            "collected_or_adjusted_during_current_biennium": "collected_or_adjusted_current_biennium"
+    }
 
-df1 = df["A62_30-en.pdf"]
-df2 = df["A63_33-en.pdf"] # larger table
+    g2 = g2.rename(columns=g2_cols)
 
-# check if df1 rows exist in df2
-merged = df1.merge(df2, on=list(df1.columns), how="left", indicator=True)
-# (COMMENTED OUT): print(merged["_merge"].value_counts())
-    # there are 90 "left_only" values, indicating df1 and df2 have different values
-
-
-# upon inspection of the original PDFs, A63_33-en.pdf contains information up to
-# 2009, while A62_30-en.pdf contains information up to 2008 - thus, the following columns
-# differ: 'collected or adjusted during current biennum', 'balance outstanding (b))',
-# and 'total outstanding (a+b)'
-
-# thus, I will just add a year to these columns and merge
-
-
-# adding year to differentiate columns
-df1.columns = [col + "_as_on_2008" if col in
-                               ["collected_adjusted_during_biennium",
-                                "balance_outstanding_a",
-                                "balance_outstanding_b",
-                                "total_outstanding_a_b"]
-                                else col for col in df1.columns]
-df2.columns = [col + "_as_on_2009" if col in
-                               ["collected_adjusted_during_biennium",
-                                "balance_outstanding_a",
-                                "balance_outstanding_b",
-                                "total_outstanding_a_b"]
-                                else col for col in df2.columns]
-
-shared_cols = list(set(df1.columns) & set(df2.columns))
-g2_merged = pd.merge(df1, df2, on=shared_cols, how='outer')
-
-def find_group2_mismatches():
-    for col in shared_cols:
-        if col != "members_and_associate_members":
-            mismatches = df1.merge(df2, on="members_and_associate_members")[
-                [f"{col}_x", f"{col}_y"]
-            ]
-            mismatches = mismatches[mismatches[f"{col}_x"] != mismatches[f"{col}_y"]]
-            if len(mismatches) > 0:
-                print(f"\n{col}:")
-                print(mismatches.head())
-
-# Two mismatches, Kyrgyzstan and Panama, are disrupting the merging
-    # Kyrgystan: differs on rescheduled assessment (31 dec 2007)
-        # reschedule assessment from a63 = balance for prior yeras 31 dec 2007 from a62
-        # gonna trust the updated one
-    # Panama: differs on balance prior years (31 dec 2007)
-        # this one is a bit more complicated, because the other numbers for collected/adjusted
-        # during biennium and balance outstanding actually match up
-        # I think I'm just gonna throw this row out
-
-g2_merged = g2_merged[g2_merged["members_and_associate_members"] != "panama"]
-
+    return g2
 
 # GROUP 3
 def clean_group3(df=df, groups=groups):
-<<<<<<< Updated upstream:src/merge_pdf_data.py
+
     g3_cols = ["contributor",
             "core_voluntary_contributions_account",
             "voluntary_contributions_core",
             "voluntary_contributions_specified",
-=======
-
-    g3_cols = ["contributor", 
-            "core_voluntary_contributions_account", 
-            "voluntary_contributions_core", 
-            "voluntary_contributions_specified", 
->>>>>>> Stashed changes:src/peritz_who-pdfs_csv-merge.py
             "stop_tb_partnership",
             "roll_back_malaria_partnership",
             "special_programmes_and_collaborative_arrangements",
@@ -198,25 +176,6 @@ def clean_group3(df=df, groups=groups):
             "grand_total",
             "water_supply_and_sanitation_collaborative_council",
             "file"]
-    
-    g3_years = {
-            "A63_ID4-en.pdf (member states)": 2009,
-            "A63_ID4-en.pdf (donors)": 2009,
-            "A64_29Add1Corr1-en.pdf": 2010,
-            "A65_29Add1-en.pdf (General)": 2010,
-            "A66_29Add1-en.pdf (General)": 2012,
-            "A68_INF1-en.pdf (General)": 2014,
-            "A69_INF3-en.pdf (General)": 2015,
-            "A70_INF4-en.pdf  (General)": 2016,
-            "A71_INF2-en.pdf  (General)": 2017,
-            "A72_INF5-en.pdf  (General)": 2018,
-            "A73_INF3-eng.pdf  (General)": 2019,
-            "a74_inf4-en.pdf (General)": 2020,
-            "A75_INF5-en.pdf (General)": 2021,
-            "a76_inf2-en.pdf (General)": 2022,
-            "A77_INF2-en.pdf (General)": 2023
-    }
-
 
     # standardize column names
     for filename in groups["group3"]:
@@ -224,14 +183,11 @@ def clean_group3(df=df, groups=groups):
         # contributor/donor column name
         df[filename] = df[filename].rename(columns={"donor": "contributor"})
 
-        # rename column - total revenue -> grand total
+        # replace "total revenue" to "grand total" in column names
         df[filename].columns = [re.sub(r"total_revenue", "grand_total", col) for col in df[filename].columns]
 
         # generalize all other column names
         df[filename] = fuzzy_rename(df[filename], g3_cols, 80)
-
-        # append "year" column
-        df[filename].loc[:, "year"] = g3_years[filename]
         
     # concatenate group 3
     g3_concat = pd.concat([df[f] for f in groups["group3"]], ignore_index=True)
@@ -243,10 +199,11 @@ def clean_group3(df=df, groups=groups):
 processed_data_dir = Path("data/processed")
 processed_data_dir.mkdir(parents=True, exist_ok=True)
 
-g1_concat = clean_group1()
-g1_concat.to_parquet(processed_data_dir / "group1.parquet")
+g1 = clean_group1()
+g1.to_parquet(processed_data_dir / "group1.parquet")
 
-g2_merged.to_parquet(processed_data_dir / "group2.parquet")
+g2 = clean_group2()
+g2.to_parquet(processed_data_dir / "group2.parquet")
 
-g3_concat = clean_group3()
-g3_concat.to_parquet(processed_data_dir / "group3.parquet")
+g3 = clean_group3()
+g3.to_parquet(processed_data_dir / "group3.parquet")
