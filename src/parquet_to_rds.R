713ca4c84e472729b6bfa5dfcb76a906db9b529d
joey suen <joeysuen66@gmail.com>
2026-06-02 14:59:19 -0700

    Add R script to export parquet to RDS file

---
+++ src/parquet_to_rds.R
@@ -0,0 +1,7 @@
+library(arrow)
+
+assessed <- read_parquet("data/processed/assessed_contributions.parquet")
+voluntary <- read_parquet("data/processed/voluntary_contributions.parquet")
+
+saveRDS(assessed, "data/processed/assessed_contributions.rds")
+saveRDS(voluntary, "data/processed/assessed_contributions.rds")
