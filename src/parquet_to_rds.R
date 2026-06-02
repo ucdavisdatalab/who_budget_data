b1e9d0d5ae5f68cd628b1c2042ad4b631c64c337
joey suen <joeysuen66@gmail.com>
2026-06-02 16:18:22 -0700

    Resolve export issue

--- src/parquet_to_rds.R
+++ src/parquet_to_rds.R
@@ -4,4 +4,4 @@ assessed <- read_parquet("data/processed/assessed_contributions.parquet")
 voluntary <- read_parquet("data/processed/voluntary_contributions.parquet")
 
 saveRDS(assessed, "data/processed/assessed_contributions.rds")
-saveRDS(voluntary, "data/processed/assessed_contributions.rds")
+saveRDS(voluntary, "data/processed/voluntary_contributions.rds")
